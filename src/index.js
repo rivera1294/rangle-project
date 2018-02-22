5c030289ea3b4c205fd3e910b6f078ff39ff1313
Jason Rivera <jrivera2850@gmail.com>
2018-02-22 04:02:40 +0000

    created a search box component to search for the robots

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 import tachyons from 'tachyons';
 
 
 render(
   <div>
+  <SearchBox/>
     <CardList robots={robots} />
   </div>
   , document.getElementById("root"));
