228eb2bc8d3c6627638874615864d1d8687a63b2
Jason Rivera <jrivera2850@gmail.com>
2018-04-05 00:55:08 +0000

    added redux to project

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div className="grow bg-light-green br3 pa3 ma2 dib">
-      <img role="presentation" src={`//robohash.org/${id}?size=200x200`} />
+      <img alt="presentation" src={`//robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom'; // glue between React and the DOM
 import App from './Containers/App';
 import './index.css';
 import 'tachyons';
+import { createStore, applyMiddleware } from 'redux';
+import { searchReducer } from './reducers';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+
+
+const logger = createLogger();
+
+const store = createStore(searchReducer, applyMiddleware(logger));
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
